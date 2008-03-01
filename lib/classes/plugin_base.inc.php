d18d97bd53f3e83ec8318f20fd01ee1db1596133
Albert Lash <albert.lash@docunext.com>
2008-03-01 23:08:27 +0000

    better organization

---
+++ lib/classes/plugin_base.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+// DELME? 
+
+class plugin_base {
+	
+	var $plugin_name;
+	var $options;
+	var $form;
+	
+	function onLoad() {
+	
+	}
+	
+	function onShow() {
+	
+	}
+	
+	function onInsert() {
+	
+	}
+	
+	function onUpdate() {
+	
+	}
+	
+	function onDelete() {
+	
+	}
+	
+	function setOptions($plugin_name, $options) {
+		$this->options = $options;
+		$this->plugin_name = $plugin_name;
+	}
+
+}
+
+?>
