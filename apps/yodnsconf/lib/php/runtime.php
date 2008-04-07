42e369b4ae50be7c378e6e1beb14ae8895939957
Albert Lash <albert.lash@docunext.com>
2008-04-07 02:12:51 +0000

    cleaning up runtime.php

--- apps/yodnsconf/lib/php/runtime.php
+++ apps/yodnsconf/lib/php/runtime.php
@@ -1,15 +1,33 @@
 <?php
+/*
+Program: YoDNSConf
+Component: runtime.php
+Copyright: Savonix Corporation
+Author: Albert L. Lash, IV
+License: Gnu Affero Public License version 3
+http://www.gnu.org/licenses
 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
-Nexista_Flow::add("pun_config",$pun_config,false);
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, see http://www.gnu.org/licenses
+or write to the Free Software Foundation,Inc., 51 Franklin Street,
+Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 # This wacky path builder is required due to mod_rewrite situations
 $path = $_SERVER['REQUEST_URI'];
 Nexista_Flow::add("request_uri",$path);
 $path = dirname($path)."/".basename($_SERVER['SCRIPT_NAME']);
 $path_prefix = dirname($path)."/";
-//$app_prefix = "acc/".APP_NAME."/";
-//$link_prefix = $path."?nid=".$app_prefix;
 $link_prefix = $path."?nid=";
 $right_now = gmdate('Y-m-d H:i:s');
 
@@ -26,10 +44,7 @@ $runtime = array('host_name'=>$_SERVER['SERVER_NAME'],
                 'username'=>$_SESSION['NX_AUTH']['username'],
                 'user_id'=>$_SESSION['NX_AUTH']['user_id'],
                 'group_id'=>$_SESSION['NX_AUTH']['group_id'],
-                'remote_ip'=>$_SERVER['REMOTE_ADDR'],
-                'debug'=>$debug,
                 'admin'=>1,
-                'top_left_logo'=>$top_left_logo,
                 'db_version'=>$db_version,
                 'timestamp'=>time());
 
