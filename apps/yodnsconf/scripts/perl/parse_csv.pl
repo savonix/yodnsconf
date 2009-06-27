3683c6f3929e3f5039993d5da5f9839350a19b24
Albert Lash <albert.lash@docunext.com>
2009-06-27 15:52:28 +0000

    update to csv parser

--- apps/yodnsconf/scripts/perl/parse_csv.pl
+++ apps/yodnsconf/scripts/perl/parse_csv.pl
@@ -2,10 +2,12 @@ use Text::CSV_XS;
 
 my $csv = Text::CSV_XS->new ({ binary => 1 });
 open my $fh, "<", "file.csv" or die "file.csv: $!";
+
 while (my $row = $csv->getline ($fh)) {
-    # do something with @$row
-   my @fields = @$row;
-    printf " %s\n",$fields[3];
+    my @fields = @$row;
+    $fields[0] =~ tr/A-Z/a-z/;
+    printf '"%s","%s"',$fields[0],$fields[2];
+    print "\n";
 }
 $csv->eof or $csv->error_diag;
 close $fh or die "file.csv: $!";
