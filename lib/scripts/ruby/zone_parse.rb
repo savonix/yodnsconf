db1899a0667d8174e123f068e6b63d43ccd06a65
Docunext <albert.lash@docunext.com>
2010-06-15 01:51:21 -0400

    figured out how to get all records

--- lib/scripts/ruby/zone_parse.rb
+++ lib/scripts/ruby/zone_parse.rb
@@ -16,8 +16,16 @@ puts "SRV: #{zf.srv.to_json}"
 puts "A4: #{zf.a4.to_json}"
 puts "MX: #{zf.mx.to_json}"
 
+rsarr = []
 zf.records.each do |res|
   #puts res unless res[0]==:soa
-  rs = zf.send res[0].to_sym unless res[0]==:soa
-  puts rs
+  unless res[0]==:soa
+    rs = zf.send res[0].to_sym
+    if rs
+      rs[0][:type] = res[0] unless rs[0].nil?
+      rsarr << rs[0] unless rs[0].nil?
+    end
+  end
 end
+
+puts rsarr.to_json
