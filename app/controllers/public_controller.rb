6d3f016394d2f9091178584f2622579a8fdd45fc
docunext <albert.lash@docunext.com>
2011-10-09 21:10:13 -0400

    merged check and index for public

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,18 +1,21 @@
 class PublicController < ApplicationController
   def index
-    @zones = Zone.find_all_by_user_id(0).paginate(:page => params[:page])
+    @zones = Zone.find_all_by_user_id(0).paginate(:page => params[:page], :per_page => 5)
+    render :check
   end
 
   def check 
-    @zone = Zone.find_by_origin("#{params[:id]}.")
-    unless @zone
-      begin
+    @zones = Zone.find_all_by_user_id(0).paginate(:page => params[:page], :per_page => 5)
+    begin
+      @zone = Zone.find_by_origin("#{params[:id]}.")
+      unless @zone
         @zone = Zone.new({:origin => "#{params[:id]}.", :ttl => 14400, :user_id => 0})
         @zone.save!
-      rescue 
-        flash[:error] = "N0t a valid domain name!"
-        redirect_to public_index_path
       end
+      @zone.resolve_ns_records
+    rescue 
+      flash[:error] = "N0t a valid domain name!"
+      redirect_to public_index_path
     end
   end
 
